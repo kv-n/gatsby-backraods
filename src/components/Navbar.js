1269be2b5014522daf5672cfeff57886285d3e7c
Kevin <k.cuison@gmail.com>
2019-06-08 14:56:23 -0700

    includes scoped css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import styles from './navbar.module.css'
 
 const Navbar = () => {
     return (
-        <div>
+        <div className={styles.navbar}>
             <h1>I AM THE NAVBAR</h1>
+            <div className={styles.links}>I'm the navbar link</div>
         </div>
     )
 }
