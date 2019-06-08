09b3a0b9d82b32b2f445450b482c5ebf02011dae
Kevin <k.cuison@gmail.com>
2019-06-08 14:01:21 -0700

    adds pages

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const error = () => {
+    return (
+        <div>
+            hello from error page
+        </div>
+    )
+}
+
+export default error
---
+++ src/pages/tours.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class tours extends Component {
+    render() {
+        return (
+            <div>
+                hello from tours page!!!
+            </div>
+        )
+    }
+}
+
