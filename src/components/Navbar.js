b3e54c90021095d766126999283c63badaa2fd66
Kevin <k.cuison@gmail.com>
2019-06-10 11:46:09 -0700

    adds navbar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'gatsby'
+import styles from '../css/navbar.module.css'
+import { FaAlignRight } from 'react-icons/fa'
+import links from '../constants/links'
+import socialIcons from '../constants/social-icons'
+import logo from '../images/logo.svg'
 
 const Navbar = () => {
+    const [isOpen, setNav] = (useState(false))
+    const toggleNav = () => {
+        setNav(isOpen => !isOpen)
+    }
+
+    console.log(isOpen)
+
     return (
-        <div>
-            <h1>I AM THE NAVBAR</h1>
-        </div>
+        <nav className={styles.navbat}>
+            <div className={styles.navCenter}>
+                <div className={styles.navHeader}>
+                    <img src={logo} alt="backgroads logo" />
+                    <button type="button" className={styles.logoBtn} onClick={toggleNav}>
+                        <FaAlignRight className={styles.logoIcon} />
+                    </button>
+                </div>
+                <ul className={isOpen ? `${styles.navLinks} ${styles.showNav}` : `${styles.navLinks}`}>
+                    {links.map((item, i) => {
+                        return (
+                            <li key={i}>
+                                <Link to={item.path}>{item.text}</Link>
+                            </li>
+                        )
+                    })}
+                </ul>
+                <div className={styles.navSocialLinks}>
+                    {socialIcons.map((item, i) => {
+                        return <a key={i} href={item.url} target="_blank" rel="noopener noreferrer">
+                            {item.icon}
+                        </a>
+                    })}
+                </div>
+            </div>
+
+        </nav>
     )
 }
 
